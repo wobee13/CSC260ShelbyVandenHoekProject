cf09bb248530707f47dcd00f3159be3859a601a0
Wobee13 <wobee13@gmail.com>
2022-11-16 14:07:31 -0600

    Intial Commit

---
+++ NftHigherOrLowerGame/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NftHigherOrLowerGame;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ NftHigherOrLowerGame/Pages/GamePage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NftHigherOrLowerGame.Pages;
+
+public partial class GamePage : ContentPage
+{
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+}
