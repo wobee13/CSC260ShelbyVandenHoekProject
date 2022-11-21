c2f951c8cdd97d00a6afe69ebef1d50f713dd37e
Shelby V <wobee13@gmail.com>
2022-11-20 18:26:33 -0600

    Added Nft Class

---
+++ NftHigherOrLowerGame/Model/Nft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NftHigherOrLowerGame.Model
+{
+
+    class Nft
+    {
+        public string Name { get; }
+        public int Id { get; }
+        public string BlurHash { get; }
+        public string ImageURL { get; }
+
+
+    }
+}
