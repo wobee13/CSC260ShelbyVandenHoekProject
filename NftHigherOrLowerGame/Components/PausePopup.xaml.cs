67080da086eab3525248369eac3ca8dd865db472
Shelby V <wobee13@gmail.com>
2022-12-07 12:51:22 -0600

    Added Pause Pop Up Menu

---
+++ NftHigherOrLowerGame/Components/PausePopup.xaml.cs
@@ -0,0 +1,12 @@
+using CommunityToolkit.Maui.Views;
+
+namespace NftHigherOrLowerGame.Components;
+
+
+public partial class PausePopup : Popup
+{
+    public PausePopup()
+    {
+        InitializeComponent();
+    }
+}
--- NftHigherOrLowerGame/Pages/MainPage.xaml.cs
+++ NftHigherOrLowerGame/Pages/MainPage.xaml.cs
@@ -1,25 +1,27 @@
 ﻿using Blurhash.SkiaSharp;
+using CommunityToolkit.Maui.Views;
+using NftHigherOrLowerGame.Components;
 using SkiaSharp;
 
 namespace NftHigherOrLowerGame.Pages;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count -= 20;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count -= 20;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
         // decode blurhash instring into a SKBitmap
         var im = Blurhasher.Decode("LEHV6nWB2yk8pyo0adR*.7kCMdnj", 4, 3);
 
@@ -27,11 +29,19 @@ public partial class MainPage : ContentPage
         var data = SKImage.FromBitmap(im).Encode();
 
         // use as a source for Image control
-		
+
         image1.Source = ImageSource.FromStream(data.AsStream);
 
         SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        DisplayPopup();
+    }
+
+    public void DisplayPopup()
+    {
+        var popup = new PausePopup();
+
+        this.ShowPopup(popup);
+    }
 }
 
 
